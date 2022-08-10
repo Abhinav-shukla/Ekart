7cbd828ce49f5f9a2aadd5da4ac670c4a8dfb477
Abhinav-shukla <diplomaticsatya@gmail.com>
2022-08-10 10:20:15 +0530

    product file is updated

--- server.js
+++ server.js
@@ -19,6 +19,44 @@ app.use("/api/login", loginRouter);
 app.use("/api/register", registerRouter);
 
 //productCreation
+app.post("/api/products", async (req, res) => {
+	const {
+		prodName: prodName,
+		category: category,
+		price: price,
+		quantity: awailQuantity,
+		imgUrl: img,
+		desciption: prodDescription,
+	} = req.body;
+
+	try {
+		const size = await Product.find().count();
+		const tempId = size + 1;
+		// console.log('db is:',size);
+		const product = await Product.create({
+			id: tempId,
+			prodName,
+			category,
+			price,
+			awailQuantity,
+			img,
+			prodDescription,
+		});
+		console.log("User created successfully: ", product);
+	} catch (error) {
+		if (error.code === 11000) {
+			// duplicate key
+			return res
+				.status(400)
+				.json({ status: "error", message: "product already in use" });
+		}
+		throw error;
+	}
+
+	res
+		.status(201)
+		.json({ status: "ok", message: "product created successfully!" });
+});
 
 const port = 3000;
 app.listen(port, "127.0.0.1", () => {
